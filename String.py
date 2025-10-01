19759f27566e5b7e2a0af33844214d52911ac5bc
muhammadfauzantob <muhammadfauzantob@gmail.com>
2025-10-01 13:57:31 +0700

    Add files via upload

---
+++ String.py
@@ -0,0 +1,13 @@
+nama_depan = "muhammad"
+nama_belakang = 'Fauzan'
+nama_lengkap = nama_depan + 'muhammad fauzan' + nama_belakang
+usia = '19'
+alamat = 'Toboali'
+kata_mutara = "jangan hanya barmimpi,wujudkamlah"
+
+print(nama_lengkap, '(' + usia + ')', ',', 'dari', alamat, ',kata mutiara:',kata_mutara)
+
+print('\nTipe dari nama_lengkap:', type(nama_lengkap))
+print('Tipe dari usia:', type(usia))
+print('Tipe dari almat:', type(alamat))
+print('Tipe dari kata_mutiara:', type(kata_mutara))
